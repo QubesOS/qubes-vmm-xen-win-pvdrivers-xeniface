2c81c089e6651958f18f773831ba85c8e2419914
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-10-09 20:43:56 +0200

    Add doxygen comments for xencontrol API

--- include/xencontrol.h
+++ include/xencontrol.h
@@ -15,9 +15,15 @@
 extern "C" {
 #endif
 
+/*! \typedef PXENCONTROL_CONTEXT
+    \brief Library handle representing a Xen Interface session
+*/
 struct _XENCONTROL_CONTEXT;
 typedef struct _XENCONTROL_CONTEXT *PXENCONTROL_CONTEXT;
 
+/*! \typedef XENCONTROL_LOG_LEVEL
+    \brief Log levels used by the library
+*/
 typedef enum
 _XENCONTROL_LOG_LEVEL {
     XLL_ERROR = 1,
@@ -27,6 +33,9 @@ _XENCONTROL_LOG_LEVEL {
     XLL_TRACE,
 } XENCONTROL_LOG_LEVEL;
 
+/*! \typedef XencontrolLogger
+    \brief Callback for receiving diagnostic messages from the library
+*/
 typedef void
 XencontrolLogger(
     IN XENCONTROL_LOG_LEVEL LogLevel,
@@ -35,6 +44,10 @@ XencontrolLogger(
     IN va_list Args
     );
 
+/*! \brief Register a callback for receiving library's diagnostic messages
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Logger Callback to register
+*/
 XENCONTROL_API
 void
 XcRegisterLogger(
@@ -42,6 +55,10 @@ XcRegisterLogger(
     IN  XencontrolLogger *Logger
     );
 
+/*! \brief Set log level threshold for library's diagnostic messages
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param LogLevel Only messages with this level and above will be sent to the logger callback
+*/
 XENCONTROL_API
 void
 XcSetLogLevel(
@@ -49,6 +66,11 @@ XcSetLogLevel(
     IN  XENCONTROL_LOG_LEVEL LogLevel
     );
 
+/*! \brief Open the Xen Interface device
+    \param Logger Callback for receiving library's diagnostic messages
+    \param Xc Xencontrol handle representing a Xen Interface session
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcOpen(
@@ -56,12 +78,23 @@ XcOpen(
     OUT PXENCONTROL_CONTEXT *Xc
     );
 
+/*! \brief Close the Xen Interface device
+    \param Xc Xencontrol handle returned by XcOpen()
+*/
 XENCONTROL_API
 void
 XcClose(
     IN  PXENCONTROL_CONTEXT Xc
     );
 
+/*! \brief Open a "listening" event channel
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param RemoteDomain ID of a remote domain that will bind the channel
+    \param Event Handle to an event that will receive event channel notifications
+    \param Mask Set to TRUE if the event channel should be initially masked
+    \param LocalPort Port number that is assigned to the event channel
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcEvtchnBindUnbound(
@@ -72,6 +105,15 @@ XcEvtchnBindUnbound(
     OUT ULONG *LocalPort
     );
 
+/*! \brief Open an event channel that was already bound by a remote domain
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param RemoteDomain ID of a remote domain that has already bound the channel
+    \param RemotePort Port number that is assigned to the event channel in the \a RemoteDomain
+    \param Event Handle to an event that will receive event channel notifications
+    \param Mask Set to TRUE if the event channel should be initially masked
+    \param LocalPort Port number that is assigned to the event channel
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcEvtchnBindInterdomain(
@@ -83,6 +125,11 @@ XcEvtchnBindInterdomain(
     OUT ULONG *LocalPort
     );
 
+/*! \brief Close an event channel
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param LocalPort Port number that is assigned to the event channel
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcEvtchnClose(
@@ -90,6 +137,11 @@ XcEvtchnClose(
     IN  ULONG LocalPort
     );
 
+/*! \brief Notify the remote end of the channel
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param LocalPort Port number that is assigned to the event channel
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcEvtchnNotify(
@@ -97,6 +149,11 @@ XcEvtchnNotify(
     IN  ULONG LocalPort
     );
 
+/*! \brief Unmask an event channel
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param LocalPort Port number that is assigned to the event channel
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcEvtchnUnmask(
@@ -104,6 +161,17 @@ XcEvtchnUnmask(
     IN  ULONG LocalPort
     );
 
+/*! \brief Grant a \a RemoteDomain permission to access local memory pages
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param RemoteDomain ID of a remote domain that is being granted access
+    \param NumberPages Number of 4k pages to grant access to
+    \param NotifyOffset Offset of a byte in the granted region that will be set to 0 when the grant is revoked
+    \param NotifyPort Local port number of an open event channel that will be notified when the grant is revoked
+    \param Flags Grant options
+    \param Address Local user mode address of the granted memory region
+    \param References An array of Xen grant numbers for every granted page
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcGnttabPermitForeignAccess(
@@ -117,6 +185,11 @@ XcGnttabPermitForeignAccess(
     OUT ULONG *References
     );
 
+/*! \brief Revoke a foreign domain access to local memory
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Address Local user mode address of the granted memory region
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcGnttabRevokeForeignAccess(
@@ -124,6 +197,17 @@ XcGnttabRevokeForeignAccess(
     IN  PVOID Address
     );
 
+/*! \brief Map a foreign memory region into the current address space
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param RemoteDomain ID of a remote domain that has granted access to the pages
+    \param NumberPages Number of 4k pages to map
+    \param References An array of Xen grant numbers for every granted page
+    \param NotifyOffset Offset of a byte in the granted region that will be set to 0 when the region is unmapped
+    \param NotifyPort Local port number of an open event channel that will be notified when the region is unmapped
+    \param Flags Map options
+    \param Address Local user mode address of the mapped memory region
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcGnttabMapForeignPages(
@@ -137,6 +221,11 @@ XcGnttabMapForeignPages(
     OUT PVOID *Address
     );
 
+/*! \brief Unmap a foreign memory region from the current address space
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Address Local user mode address of the mapped memory region
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcGnttabUnmapForeignPages(
@@ -144,15 +233,28 @@ XcGnttabUnmapForeignPages(
     IN  PVOID Address
     );
 
+/*! \brief Read a XenStore key
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Path Path to the key
+    \param cbValue Size of the \a Value buffer, in bytes
+    \param Value Buffer that receives the value
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreRead(
     IN  PXENCONTROL_CONTEXT Xc,
     IN  PCHAR Path,
-    IN  DWORD cbOutput,
-    OUT CHAR *Output
+    IN  DWORD cbValue,
+    OUT CHAR *Value
     );
 
+/*! \brief Write a value to a XenStore key
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Path Path to the key
+    \param Value Value to write
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreWrite(
@@ -161,6 +263,13 @@ XcStoreWrite(
     IN  PCHAR Value
     );
 
+/*! \brief Enumerate all immediate child keys of a XenStore key
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Path Path to the key
+    \param cbOutput Size of the \a Output buffer, in bytes
+    \param Output Buffer that receives a NUL-separated child key names
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreDirectory(
@@ -170,6 +279,11 @@ XcStoreDirectory(
     OUT CHAR *Output
     );
 
+/*! \brief Remove a XenStore key
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Path Path to the key
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreRemove(
@@ -177,6 +291,13 @@ XcStoreRemove(
     IN  PCHAR Path
     );
 
+/*! \brief Set permissions of a XenStore key
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Path Path to the key
+    \param Count Number of permissions
+    \param Permissions Array of permissions to set
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreSetPermissions(
@@ -186,6 +307,13 @@ XcStoreSetPermissions(
     IN  PXENBUS_STORE_PERMISSION Permissions
     );
 
+/*! \brief Add a XenStore key watch
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Path Path to the key to be watched
+    \param Event Handle to an event that will be signaled when the watch fires
+    \param Handle An opaque value representing the watch
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreAddWatch(
@@ -195,6 +323,11 @@ XcStoreAddWatch(
     OUT PVOID *Handle
     );
 
+/*! \brief Remove a XenStore watch
+    \param Xc Xencontrol handle returned by XcOpen()
+    \param Handle Watch handle returned by XcStoreAddWatch()
+    \return Error code
+*/
 XENCONTROL_API
 DWORD
 XcStoreRemoveWatch(
